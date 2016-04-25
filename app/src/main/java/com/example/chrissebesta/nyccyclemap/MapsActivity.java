a66164d42ec0fc34cdfd0c773a63e6ab530ad2b6
zebesta <chris.sebesta@gmail.com>
2016-04-25 15:59:30 -0400

    Got the map up and running and added a ghost bike icon from googles material design icon library to represent data, want to make a white bike for deaths to reference ghost bikes and a red bike for injuries

--- app/src/main/java/com/example/chrissebesta/nyccyclemap/MapsActivity.java
+++ app/src/main/java/com/example/chrissebesta/nyccyclemap/MapsActivity.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.example.chrissebesta.nyccyclemap.data.CycleContract;
 import com.example.chrissebesta.nyccyclemap.data.CycleDbHelper;
@@ -11,10 +12,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    public final String LOG_TAG = MapsActivity.class.getSimpleName();
 
     private GoogleMap mMap;
 
@@ -44,13 +47,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //Create cursor to cycle through cycle injury databse data
         CycleDbHelper helper = new CycleDbHelper(getBaseContext());
         SQLiteDatabase db = helper.getWritableDatabase();
+
+        //TODO update this to only query the database for a limited lat and long region around current maps focus to limit work here
         Cursor cursor = db.rawQuery("SELECT * FROM " + CycleContract.CycleEntry.TABLE_NAME, null);
         cursor.moveToFirst();
-        
+
+        //Add locations for each point in the
+        for (int i = 0; i < cursor.getCount(); i++) {
+            String boroughName = cursor.getString(cursor.getColumnIndex(CycleContract.CycleEntry.COLUMN_BOROUGH));
+            //String contributingFactor = cursor.getString(cursor.getColumnIndex(CycleContract.CycleEntry.COLUMN_CONTRIBUTING_FACTOR_VEHICLE_1));
+            LatLng latLng = new LatLng(cursor.getDouble(cursor.getColumnIndex(CycleContract.CycleEntry.COLUMN_LATITUDE)), cursor.getDouble(cursor.getColumnIndex(CycleContract.CycleEntry.COLUMN_LONGITUDE)));
+            Log.d(LOG_TAG, "Adding point to map at: " + latLng.toString());
+            mMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_directions_bike_white_24dp))
+                    .title(boroughName)
+                    .position(latLng));
+            cursor.moveToNext();
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+        cursor.close();
+
     }
 }
